b231c16ecefa37186b52612ce1cbdd3a552e8ef7
Ellis Kenyo <elken@member.fsf.org>
2017-06-10 16:40:06 +0100

    Config stuff

--- project.clj
+++ project.clj
@@ -2,12 +2,16 @@
   :description "Clojure version of the League of Legends API"
   :url "https://elken.github.io/lol-api"
   :license {:name "BSD 3 Clause"
-            :url "https://opensource.org/licenses/BSD-3-Clause"}
+            :url  "https://opensource.org/licenses/BSD-3-Clause"}
   :plugins [[lein-codox "0.10.3"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [yogthos/config "0.8"]]
+                 [org.clojure/core.incubator "0.1.4"]
+                 [yogthos/config "0.8"]
+                 [slingshot "0.12.2"]
+                 [clj-http "3.6.1"]
+                 [cheshire "5.7.1"]]
   :codox
   {:output-path "../docs"
-   :metadata {:doc/format :markdown}
-   :source-uri "http://github.com/elken/lol-api/blob/master/{filepath}#L{line}"}
-  :profiles {:dev  {:resource-paths ["config"]}})
+   :metadata    {:doc/format :markdown}
+   :source-uri  "http://github.com/elken/lol-api/blob/master/{filepath}#L{line}"}
+  :profiles {:dev {:resource-paths ["config"]}})
