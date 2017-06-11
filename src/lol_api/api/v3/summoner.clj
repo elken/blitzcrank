ee8bdc2b17879ffb2be50076f8cfb430e0653d06
Ellis Kenyo <elken@member.fsf.org>
2017-06-11 18:59:06 +0100

    Summoner refactor
    Moved to a slightly more sensible API declaration

--- src/lol_api/api/v3/summoner.clj
+++ src/lol_api/api/v3/summoner.clj
@@ -1,52 +1,30 @@
 (ns lol-api.api.v3.summoner
   "API methods for [summoner-v3](https://developer.riotgames.com/api-methods/#summoner-v3)"
-  (:require [lol-api.models.summoner :as model]
-            [lol-api.api.v3.api :as api]
-            [lol-api.env :as env]
-            [cheshire.core :refer :all]))
-
-(defn- json-to-summoner
-  "Convert a JSON response to a summoner model"
-  [json]
-  (let [json (parse-string (:body json) true)]
-    (model/map->Summoner {:icon-id       (:profileIconId json)
-                          :name          (:name json)
-                          :level         (:summonerLevel json)
-                          :revision-date (:revisionDate json)
-                          :id            (:id json)
-                          :account-id    (:accountId json)})))
+  (:require [lol-api.api.v3.api :as api]
+            [clojure.core.strint :refer [<<]]
+            [lol-api.env :as env]))
 
 (defn get-summoner-by-name
   "Get a summoner by summoner name."
-  ([name]
-   (get-summoner-by-name name (env/get-region)))
-  ([name region]
-   (json-to-summoner (api/get-resource-url "summoner" :route "summoners/by-name" :region region :path-params name))))
+  [name & [options]]
+  (api/get-resource-body "summoner" "summoners/by-name" (merge {:path-params name} options)))
 
 (defn get-summoner-by-account-id
   "Get a summoner by account ID"
-  ([account-id]
-   (get-summoner-by-account-id account-id (env/get-region)))
-  ([account-id region]
-   (json-to-summoner (api/get-resource-url "summoner" :route "summoners/by-account" :region region :path-params account-id))))
+  [account-id & [options]]
+  (api/get-resource-body "summoner" "summoners/by-account" (merge {:path-params account-id} options)))
 
 (defn get-summoner-by-summoner-id
   "Get a summoner by summoner ID"
-  ([id]
-   (get-summoner-by-summoner-id id (env/get-region)))
-  ([id region]
-   (json-to-summoner (api/get-resource-url "summoner" :route "summoners" :region region :path-params id))))
+  [id & [options]]
+  (api/get-resource-body "summoner" "summoners" (merge {:path-params id} options)))
 
 (defn get-account-id-by-name
   "Get the account ID by summoner name"
-  ([name]
-   (:account-id (get-summoner-by-name name (env/get-region))))
-  ([name region]
-   (:account-id (get-summoner-by-name name region))))
+  [name & [options]]
+  (:account-id (get-summoner-by-name name options)))
 
 (defn get-summoner-id-by-name
   "Get the summoner ID by summoner name"
-  ([name]
-   (:id (get-summoner-by-name name (env/get-region))))
-  ([name region]
-   (:id (get-summoner-by-name name region))))
+  [name & [options]]
+  (:id (get-summoner-by-name name options)))
