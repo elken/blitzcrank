7a4610e03fbb3fa643b5d3c0f74e1e6a75f94b04
Ellis Kenyo <elken@member.fsf.org>
2017-06-11 19:00:14 +0100

    Implemented basic mastery methods
    Added methods to get mastery pages by summoner Id and name

--- src/lol_api/api/v3/masteries.clj
+++ src/lol_api/api/v3/masteries.clj
@@ -1,1 +1,14 @@
-(ns lol-api.api.v3.masteries)
+(ns lol-api.api.v3.masteries
+  "API methods for [masteries-v3](https://developer.riotgames.com/api-methods/#masteries-v3)"
+  (:require [lol-api.api.v3.api :as api]
+            [lol-api.api.v3.summoner :as summoner]))
+
+(defn get-mastery-pages-by-summoner-id
+  "Get all mastery pages for a summoner by summoner ID"
+  [summoner-id & [options]]
+  (api/get-resource-body "platform" "masteries/by-summoner" (merge {:path-params summoner-id} options)))
+
+(defn get-mastery-pages-by-summoner-name
+  "Get all mastery pages for a summoner by summoner name"
+  [summoner-name & [options]]
+  (get-mastery-pages-by-summoner-id (summoner/get-summoner-id-by-name summoner-name) options))
