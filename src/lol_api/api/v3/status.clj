aae08dcfa2746106b00d0979e0e9f05e097e065f
Ellis Kenyo <elken@member.fsf.org>
2017-06-11 18:59:42 +0100

    Implemented status
    Added methods for checking API status for all regions and a specific region

--- src/lol_api/api/v3/status.clj
+++ src/lol_api/api/v3/status.clj
@@ -1,2 +1,14 @@
-(ns lol-api.api.v3.status)
+(ns lol-api.api.v3.status
+  "API methods for [status-v3](https://developer.riotgames.com/api-methods/#lol-status-v3)"
+  (:require [lol-api.api.v3.api :as api]
+            [lol-api.util :refer [region-codes]]))
 
+(defn get-all-endpoints-for-region
+  "Get API status for all the endpoints in a given region"
+  [region & [options]]
+  (api/get-resource-body "status" "shard-data" (merge {:region region} options)))
+
+(defn get-all-endpoints
+  "Get API status for all endpoints in all regions"
+  []
+  (map get-all-endpoints-for-region (remove #(= %1 "na") (region-codes))))
