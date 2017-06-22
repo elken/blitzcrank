7536d67b2ac76daa221c295ac7aeafdcf8d35751
Ellis Kenyo <elken@member.fsf.org>
2017-06-22 10:01:22 +0100

    Implemented champion-v3

--- src/blitzcrank/endpoints/champion.clj
+++ src/blitzcrank/endpoints/champion.clj
@@ -2,4 +2,37 @@
   "API methods for [champion-v3](https://developer.riotgames.com/api-methods/#champion-v3)"
   (:require [blitzcrank.endpoints.api :as api]))
 
-(defn all)
+(defn all
+  "Get all champions"
+  [& [options]]
+  (api/resource-body "platform" "champions" options))
+
+(defn by-id
+  "Get a champion by champion ID"
+  [id & [options]]
+  (api/resource-body "platform" "champions" (merge {:path-params id} options)))
+
+(defn all-ranked
+  "Get all champions allowed in ranked"
+  [& [options]]
+  (filter #(:rankedPlayEnabled %) (:champions (all options))))
+
+(defn all-bots
+  "Get all champions allowed as bots in custom games"
+  [& [options]]
+  (filter #(:botEnabled %) (:champions (all options))))
+
+(defn all-free
+  "Get all champions that are currently in the free rotation"
+  [& [options]]
+  (filter #(:freeToPlay %) (:champions (all options))))
+
+(defn all-custom-bots
+  "Get all champions allowed as bots in matchmade games"
+  [& [options]]
+  (filter #(:botMmEnabled %) (:champions (all options))))
+
+(defn all-active
+  "Get all champions that are active"
+  [& [options]]
+  (filter #(:active %) (:champions (all options))))
