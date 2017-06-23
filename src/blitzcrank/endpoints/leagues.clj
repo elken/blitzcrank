89d2712d58a419bfbf166f104fd0f0e13f1adc93
Ellis Kenyo <elken@mykolab.com>
2017-06-23 21:03:34 +0100

    Implemented leagues

--- src/blitzcrank/endpoints/leagues.clj
+++ src/blitzcrank/endpoints/leagues.clj
@@ -1,1 +1,24 @@
-(ns blitzcrank.endpoints.leagues)
+(ns blitzcrank.endpoints.leagues
+  "API methods for [leagues-v3](https://developer.riotgames.com/api-methods/#league-v3)"
+  (:require [blitzcrank.endpoints.api :as api]
+            [clojure.core.strint :refer [<<]]))
+
+(defn challengers-for-queue
+  "Get all the challenger players for queue"
+  [queue-id & [options]]
+  (api/resource-body "league" (<< "challengerleagues/by-queue/~{queue-id}") options))
+
+(defn leagues-for-summoner
+  "Get leagues in all queues for a given summoner ID"
+  [summoner-id & [options]]
+  (api/resource-body "league" (<< "leagues/by-summoner/~{summoner-id}") options))
+
+(defn masters-for-queue
+  "Get all the master players for given queue"
+  [queue-id & [options]]
+  (api/resource-body "league" (<< "masterleagues/by-queue/~{queue-id}") options))
+
+(defn league-positions-for-summoner
+  "Get league positions in all queues for a given summoner ID"
+  [summoner-id & [options]]
+  (api/resource-body "league" (<< "positions/by-summoner/~{summoner-id}") options))
